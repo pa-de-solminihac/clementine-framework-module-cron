bf77a18d3be6ff7c4efc5bbadb37c673b270bb3d
Pierre-Alexis de Solminihac <pa@solminihac.fr>
2013-02-05 00:47:06 +0100

    restoring cron-1.0 branch

--- ctrl/cronCronController.php
+++ ctrl/cronCronController.php
@@ -13,40 +13,30 @@ class cronCronController extends cronCronController_Parent
 
     public $crontask = array();
 
-    public function __construct($request, $params = null)
+    public function __construct($params = null)
     {
-        $cron_config = Clementine::$config['clementine_cron'];
-        if ($request->METHOD == 'CLI'
-            || !isset($cron_config['allowed_ip']) 
-            || (isset($cron_config['allowed_ip']) && (!$cron_config['allowed_ip'] || (in_array($_SERVER['REMOTE_ADDR'], explode(',', $cron_config['allowed_ip'])))))) {
-            // no time limit
-            ini_set('max_execution_time', 0);
-            // be quiet
-            define('__NO_DEBUG_DIV__', 1);
-            // get action info in order to log start and stop date
-            $req = $this->getRequest();
-            $this->crontask['lang']       = $req->LANG;
-            $this->crontask['action']     = $req->ACT;
-            $this->crontask['date_start'] = date('Y-m-d H:i:s');
-            $this->crontask['date_stop']  = null;
-            // log start date
-            if (!isset($this->crontask['logging'])) {
-                $this->crontask['logging'] = 1;
-                $cron = $this->getModel('cron');
-                $this->crontask['id'] = $cron->logging($this->crontask);
-            }
-        } else {
-            // forbidden for this calling IP
-            header($_SERVER['SERVER_PROTOCOL'] . ' 403 Forbidden', true);
-            echo 'Forbidden';
-            die();
+        // no time limit
+        ini_set('max_execution_time', 0);
+        // be quiet
+        define('__NO_DEBUG_DIV__', 1);
+        // get action info in order to log start and stop date
+        $req = $this->getRequest();
+        $this->crontask['lang']       = $req['LANG'];
+        $this->crontask['action']     = $req['ACT'];
+        $this->crontask['date_start'] = date('Y-m-d H:i:s');
+        $this->crontask['date_stop']  = null;
+        // log start date
+        if (!isset($this->crontask['logging'])) {
+            $this->crontask['logging'] = 1;
+            $cron = $this->getModel('cron');
+            $this->crontask['id'] = $cron->logging($this->crontask);
         }
     }
 
     public function __destruct()
     {
         // log end date
-        if (isset($this->crontask['logging']) && $this->crontask['logging']) {
+        if ($this->crontask['logging']) {
             $this->crontask['date_stop'] = date('Y-m-d H:i:s');
             $cron = $this->getModel('cron');
             return $cron->logging($this->crontask);
@@ -59,7 +49,7 @@ class cronCronController extends cronCronController_Parent
      * @access public
      * @return void
      */
-    public function indexAction($request, $params = null)
+    public function indexAction($params = null)
     {
         // be quiet
         return array('dont_getblock' => true);
